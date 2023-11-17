8629d96afbb9f59f066ed4065c055bbd827812fa
Plopmenz <plopmenz@gmail.com>
2023-11-17 18:51:34 +0100

    removed direct rpc event filter code

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,8 +5,6 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export const blocknumber = BigInt(42473706)
-
 export async function asyncMap<T1, T2>(
   arr: T1[] | readonly T1[],
   func: (elem: T1) => Promise<T2>
