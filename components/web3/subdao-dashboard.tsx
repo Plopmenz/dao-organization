3d3a44f2b27bdf5a1690c957e018d552d2275298
Plopmenz <plopmenz@gmail.com>
2023-11-21 19:18:26 +0100

    subdao backend not receiving events

--- components/web3/subdao-dashboard.tsx
+++ components/web3/subdao-dashboard.tsx
@@ -1,35 +1,42 @@
 "use client"
 
-import { useEffect, useState } from "react"
+// import { useEffect, useState } from "react"
 import { Address } from "viem"
+import { useContractRead } from "wagmi"
 
-import { getSubDAO } from "@/lib/backend"
+// import { getSubDAO } from "@/lib/backend"
+import OpenRD from "@/lib/OpenR&D"
 import { ViewOrganization } from "@/components/web3/view-organization"
 
 export function SubDAODashboard({ subDAO }: { subDAO: Address | undefined }) {
-  const [subDAOs, setSubDAOs] = useState<Address[]>([])
+  // const [subDAOs, setSubDAOs] = useState<Address[]>([])
 
-  useEffect(() => {
-    const fetch = async () => {
-      if (!subDAO) {
-        setSubDAOs([])
-      } else {
-        const sharedAddressInfo = await getSubDAO(subDAO)
-        setSubDAOs(sharedAddressInfo.subdaos)
-      }
-    }
+  // useEffect(() => {
+  //   const fetch = async () => {
+  //     if (!subDAO) {
+  //       setSubDAOs([])
+  //     } else {
+  //       const sharedAddressInfo = await getSubDAO(subDAO)
+  //       setSubDAOs(sharedAddressInfo.subdaos)
+  //     }
+  //   }
 
-    fetch().catch(console.error)
-  }, [subDAO])
+  //   fetch().catch(console.error)
+  // }, [subDAO])
+
+  // Backend not receiving subDAO events for some reason
+  const { data: subDAOs } = useContractRead({
+    abi: OpenRD.contracts.SubDAOImplementation.abi,
+    address: subDAO,
+    functionName: "getSubDAOs",
+  })
 
   return (
     <div>
       <h1 className="text-1xl font-extrabold leading-tight tracking-tighter md:text-2xl">
         Departments
       </h1>
-      {subDAOs.map((d, i) => (
-        <ViewOrganization organization={d} key={i} />
-      ))}
+      {subDAOs?.map((d, i) => <ViewOrganization organization={d} key={i} />)}
     </div>
   )
 }
